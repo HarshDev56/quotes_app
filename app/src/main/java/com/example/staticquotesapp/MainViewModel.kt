52260971766d1f7dc75a1de50834dbdd2c0b0320
HarshDev56 <harshfichadiya99@gmail.com>
2023-01-13 01:02:05 +0530

    done with normal quote app which can share quote in other apps

--- app/src/main/java/com/example/staticquotesapp/MainViewModel.kt
+++ app/src/main/java/com/example/staticquotesapp/MainViewModel.kt
@@ -2,10 +2,11 @@ package com.example.staticquotesapp
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
 
 class MainViewModel(val contex: Context) : ViewModel() {
-    private val quoteList: Array<QuoteModel> = emptyArray()
-    private val index = 0
+    private var quoteList: Array<QuoteModel> = emptyArray()
+    private var index = 0
 
     init {
         quoteList = loadQuoteFromAssests()
@@ -17,5 +18,15 @@ class MainViewModel(val contex: Context) : ViewModel() {
         val buffer = ByteArray(size)
         inputSystem.read(buffer)
         inputSystem.close()
+        val json = String(buffer,Charsets.UTF_8)
+        val gson = Gson()
+        return gson.fromJson(json,Array<QuoteModel>::class.java)
     }
+
+    fun getQuote() = quoteList[index]
+
+    fun nextQuote() = quoteList[++index]
+
+    fun previousQuote() = quoteList[--index]
+
 }
